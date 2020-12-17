e98aa392243c824388b3098a0f48cd36ba563c1d
annoming <annoming_2899@163.com>
2020-12-17 16:38:20 +0800

    feat:为登录页设置图片背景
    成功设置登录页图片大小，暂时无法解决等比例缩放，采取的是直接禁止滚动只截取图片的一部分

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <div id="app">
+    <!-- 登录页背景图片  -->
+    <div class="login-img">
+      <img src="../static/images/profittrea.jpg" alt="图片正在加载" />
+    </div>
   </div>
 </template>
 
 <style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  .login-img{
+    width: 100%;
+    height: 50rem;
+    /* 控制禁止滚动 */
+    overflow: hidden;
+  }
+  .login-img img{
+    width: 100%;
+    height: auto;
+  }
+  /*
+    不允许空行否则会报错
+    Trailing spaces not allowed no-trailing-spaces
+  */
 </style>

b38ae38df7c6f733762d5e16d7e2fd3f8d1f4ff1
annoming <annoming_2899@163.com>
2020-12-17 19:14:40 +0800

    这是一次错误的commit

---
+++ src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="index">
+    <!-- 登录页背景图片  -->
+    <topbar/>
+    <img src="../../static/images/profittrea.jpg" alt="图片正在加载中……" />
+  </div>
+</template>
+<script>
+import topbar from '@/components/topbar.vue'
+export default {
+  name: 'index',
+  components: {
+    topbar
+  }
+}
+</script>
+
+<style scoped>
+img{
+  width: 100%;
+  height: auto;
+}
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="login">
+    <!-- 登录页背景图片  -->
+    <div class="login-img">
+      <img src="../static/images/profittrea.jpg" alt="图片正在加载中……" />
+    </div>
+  </div>
+</template>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  .login-img{
+    width: 100%;
+    height: 50rem;
+    /* 控制禁止滚动 */
+    overflow: hidden;
+  }
+  .login-img img{
+    width: 100%;
+    height: auto;
+  }
+  /*
+    不允许空行否则会报错
+    Trailing spaces not allowed no-trailing-spaces
+  */
+</style>

a286deb8dabba3fb5cbc00e2b760de4f8692df55
annoming <annoming_2899@163.com>
2020-12-22 13:52:06 +0800

    style:微调市场页布局，添加title

--- src/views/market.vue
+++ src/views/market.vue
@@ -2,6 +2,7 @@
   <div class="market">
     <topbar></topbar>
     <div class="m-main">
+      <h2 class="market-title">高流动性的市场让你具有更多的可能性</h2>
       <Warning></Warning>
       <Explore></Explore>
     </div>
@@ -28,9 +29,16 @@ export default {
 <style>
 .m-main{
   width: 100%;
-  height: 1000px;
+  height: 1600px;
   background-size: 100% auto;
   background-image: url(../../static/images/profittrea.jpg);
   margin-bottom: 100px;
+}
+.market-title{
+  width: 600px;
+  font-size: 45px;
+  color: white;
+  margin: 0 0 120px 80px;
+  padding-top: 200px;
 }
 </style>

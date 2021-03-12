9ebea9d9ff7cf28f71bda8292c208080762cfdf2
annoming <annoming_2899@163.com>
2021-03-12 15:53:10 +0800

    更新market页面代码并优化样式

--- src/components/market/Explore.vue
+++ src/components/market/Explore.vue
@@ -1,18 +1,240 @@
 <template>
   <div class="explore">
-    <span>探索我们的市场和交易</span>
-    <div class="e-main">
-      <div class="e-main-block">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="e-main-block">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+    <div class="explore-title">
+      <h1>探索我们的市场和交易</h1>
     </div>
+    <el-row>
+      <el-col :span="11" class="explore-block">
+        <div class="explore-content">
+          <el-row>
+            <el-col :span="15" class="ec-left">
+              <div class="content-title">
+                <h3>
+                  <a href="">
+                    <span>股票</span>
+                    <i class="el-icon-arrow-right"></i>
+                  </a>
+                </h3>
+              </div>
+              <div class="content-word">
+                <p>管理您的股指敞口，并在领先的股指期货和期权市场中发现新机会，提供基于全球基准指数的产品，包括S＆P 500，Nasdaq-100，Russell 2000和Nikkei 225。</p>
+              </div>
+            </el-col>
+            <el-col :span="8" class="ec-right">
+              <el-row>
+                <el-col>
+                  <div>
+                    <h5>平均每日交易量</h5>
+                    <h5 class="ec-right-data">160万</h5>
+                  </div>
+                  <div class="ec-right-block">
+                    <h5>每日平均名义价值</h5>
+                    <h5 class="ec-right-data">7171亿</h5>
+                  </div>
+                </el-col>
+                <el-col>
+                  <div class="ec-right-block">
+                    <h5>特色产品</h5>
+                    <p class="ec-right-product">
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你S＆P 500</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你 NASDAQ-100</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>微型E-迷你</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你Russell 2000</font>
+                      </a>
+                    </p>
+                  </div>
+                  <div>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+      <el-col :span="11" class="explore-block eb-right">
+        <div class="explore-content">
+          <el-row>
+            <el-col :span="15" class="ec-left">
+              <div class="content-title">
+                <h3>
+                  <a href="">
+                    <span>股票</span>
+                    <i class="el-icon-arrow-right"></i>
+                  </a>
+                </h3>
+              </div>
+              <div class="content-word">
+                <p>管理您的股指敞口，并在领先的股指期货和期权市场中发现新机会，提供基于全球基准指数的产品，包括S＆P 500，Nasdaq-100，Russell 2000和Nikkei 225。</p>
+              </div>
+            </el-col>
+            <el-col :span="8" class="ec-right">
+              <el-row>
+                <el-col>
+                  <div>
+                    <h5>平均每日交易量</h5>
+                    <h5 class="ec-right-data">160万</h5>
+                  </div>
+                  <div class="ec-right-block">
+                    <h5>每日平均名义价值</h5>
+                    <h5 class="ec-right-data">7171亿</h5>
+                  </div>
+                </el-col>
+                <el-col>
+                  <div class="ec-right-block">
+                    <h5>特色产品</h5>
+                    <p class="ec-right-product">
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你S＆P 500</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你 NASDAQ-100</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>微型E-迷你</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你Russell 2000</font>
+                      </a>
+                    </p>
+                  </div>
+                  <div>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="11" class="explore-block">
+        <div class="explore-content">
+          <el-row>
+            <el-col :span="15" class="ec-left">
+              <div class="content-title">
+                <h3>
+                  <a href="">
+                    <span>股票</span>
+                    <i class="el-icon-arrow-right"></i>
+                  </a>
+                </h3>
+              </div>
+              <div class="content-word">
+                <p>管理您的股指敞口，并在领先的股指期货和期权市场中发现新机会，提供基于全球基准指数的产品，包括S＆P 500，Nasdaq-100，Russell 2000和Nikkei 225。</p>
+              </div>
+            </el-col>
+            <el-col :span="8" class="ec-right">
+              <el-row>
+                <el-col>
+                  <div>
+                    <h5>平均每日交易量</h5>
+                    <h5 class="ec-right-data">160万</h5>
+                  </div>
+                  <div class="ec-right-block">
+                    <h5>每日平均名义价值</h5>
+                    <h5 class="ec-right-data">7171亿</h5>
+                  </div>
+                </el-col>
+                <el-col>
+                  <div class="ec-right-block">
+                    <h5>特色产品</h5>
+                    <p class="ec-right-product">
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你S＆P 500</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你 NASDAQ-100</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>微型E-迷你</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你Russell 2000</font>
+                      </a>
+                    </p>
+                  </div>
+                  <div>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+      <el-col :span="11" class="explore-block eb-right">
+        <div class="explore-content">
+          <el-row>
+            <el-col :span="15" class="ec-left">
+              <div class="content-title">
+                <h3>
+                  <a href="">
+                    <span>股票</span>
+                    <i class="el-icon-arrow-right"></i>
+                  </a>
+                </h3>
+              </div>
+              <div class="content-word">
+                <p>管理您的股指敞口，并在领先的股指期货和期权市场中发现新机会，提供基于全球基准指数的产品，包括S＆P 500，Nasdaq-100，Russell 2000和Nikkei 225。</p>
+              </div>
+            </el-col>
+            <el-col :span="8" class="ec-right">
+              <el-row>
+                <el-col>
+                  <div>
+                    <h5>平均每日交易量</h5>
+                    <h5 class="ec-right-data">160万</h5>
+                  </div>
+                  <div class="ec-right-block">
+                    <h5>每日平均名义价值</h5>
+                    <h5 class="ec-right-data">7171亿</h5>
+                  </div>
+                </el-col>
+                <el-col>
+                  <div class="ec-right-block">
+                    <h5>特色产品</h5>
+                    <p class="ec-right-product">
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你S＆P 500</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你 NASDAQ-100</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>微型E-迷你</font>
+                      </a>
+                      <br/>
+                      <a href="" class="ec-right-link">
+                        <font>E-迷你Russell 2000</font>
+                      </a>
+                    </p>
+                  </div>
+                  <div>
+                  </div>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -23,33 +245,66 @@
 .explore{
   height: 830px;
   width: 90%;
-  background-color: rgba(205,51,51,80%);
+  color: white;
+  /* background-color: rgba(205,51,51,80%); */
   margin: 80px auto;
   border-radius: 10px;
 }
-.explore > span{
-  display: inline-block;
-  color: white;
-  font-size: 30px;
-  margin: 30px 0 0 50px;
+.explore-title{
+  margin-top: -20px;
 }
-.e-main{
-  width: 93.5%;
-  height: 80%;
-  margin: auto;
+.eb-right{
+  margin-left: 95px;
 }
-.e-main-block{
-  margin-top: 30px;
+.explore-block{
+  height: 85%;
+  margin-top: 40px;
+  background-color: rgb(205,38,38,73%);
+}
+.el-row{
   width: 100%;
   height: 50%;
+  color: white;
+
 }
-.e-main-block > div{
-  width: 30%;
-  height: 90%;
-  float: left;
-  background-color: white;
+.el-row .el-col{
+  /* padding-left: 30px; */
+
 }
-.e-main-block > div:not(:first-child){
-  margin-left: 5%;
+.content-title{
+  margin: 30px 0px 0px 20px;
+  font-size: 25px;
+  font-weight: 100;
+}
+.content-word{
+  padding:0px 20px;
+  margin-top: 20px;
+}
+.ec-left{
+  margin-left: 20px;
+  height: 180px;
+}
+.ec-right{
+  margin-top: 30px;
+  padding-left: 20px;
+  border-left: 1px solid #EE6363;
+}
+.ec-right-data{
+  margin-top: 5px;
+  font-size: 25px;
+}
+.ec-right-product{
+  font-size: 16px;
+}
+.ec-right-block{
+  margin-top: 10px;
+}
+.ec-right-link{
+  display: inline-block;
+  margin-top: 10px;
+}
+.explore-content a{
+  text-decoration: none;
+  color: white;
 }
 </style>
--- src/components/market/Warning.vue
+++ src/components/market/Warning.vue
@@ -1,33 +1,91 @@
 <template>
   <div class="warning">
-    <span id="w-title">市场预警</span>
-    <div>
-      <div class="w-main1">
-        <span id="nearest">最近的预警</span>
-        <span id="show-all">查看全部</span>
-      </div>
-      <div class="w-main2">
-        <div class="warning-content">
-          <i class="el-icon-bottom change"></i>
-          <span>E-迷你纳斯达克</span>
-          <p class="change">较上一交易日下跌1%</p>
-          <span class="change">3天前</span>
+    <div class="warning-title">
+      <h3 class="w-word">市场预警</h3>
+    </div>
+    <div class="warning-container">
+      <div class="warning-header">
+        <div class="wc-title">
+          <span>
+            最近的
+          </span>
+          <br/>
+          <span>预警</span>
         </div>
+        <a href="" class="show-all">查看全部</a>
       </div>
-      <div class="w-main2">
-        <div class="warning-content">
-          <i class="el-icon-bottom change"></i>
-          <span class="change">E-迷你纳斯达克</span>
-          <p class="change">较上一交易日下跌1%</p>
-          <span class="change">3天前</span>
+      <div class="warning-slider">
+        <div class="ws-content">
+          <a href="" class="ws-link">
+            <div class="ms-element">
+              <div class="movement">
+                <span class="el-icon-bottom"></span>
+              </div>
+              <div class="details">
+                <div class="product-name">
+                  <font>
+                    E-迷你纳斯达克100
+                  </font>
+                </div>
+                <div class="product-text">
+                  <font>较上一交易日上涨2％</font>
+                </div>
+                <div class="last-update">
+                  <font>
+                    一天前
+                  </font>
+                </div>
+              </div>
+            </div>
+          </a>
         </div>
-      </div>
-      <div class="w-main2">
-        <div class="warning-content">
-          <i class="el-icon-bottom change"></i>
-          <span class="change">E-迷你纳斯达克</span>
-          <p class="change">较上一交易日下跌1%</p>
-          <span class="change">3天前</span>
+        <div class="ws-content">
+          <a href="" class="ws-link">
+            <div class="ms-element">
+              <div class="movement">
+                <span class="el-icon-bottom"></span>
+              </div>
+              <div class="details">
+                <div class="product-name">
+                  <font>
+                    E-迷你纳斯达克100
+                  </font>
+                </div>
+                <div class="product-text">
+                  <font>较上一交易日上涨2％</font>
+                </div>
+                <div class="last-update">
+                  <font>
+                    一天前
+                  </font>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="ws-content">
+          <a href="" class="ws-link">
+            <div class="ms-element">
+              <div class="movement">
+                <span class="el-icon-bottom"></span>
+              </div>
+              <div class="details">
+                <div class="product-name">
+                  <font>
+                    E-迷你纳斯达克100
+                  </font>
+                </div>
+                <div class="product-text">
+                  <font>较上一交易日上涨2％</font>
+                </div>
+                <div class="last-update">
+                  <font>
+                    一天前
+                  </font>
+                </div>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
@@ -44,51 +102,70 @@
   background-color: white;
   margin: auto;
   border-radius: 10px;
+  color: #5a6874;
 }
-#w-title{
-  display: inline-block;
-  margin: 20px 0 15px 65px;
-  font-size: 26px;
+.warning-title{
+  font-size: 20px;
+  padding:2.5rem 0px 0px 1.875rem;
 }
-#nearest{
-  display: block;
-  margin-left: 65px;
-  font-size: 17px;
-  width: 60px;
-}
-#show-all{
-  margin: 30px 0 0 65px;
-  font-size: 13px;
-  display: block;
-  color: #CD0000;
-}
-.w-main1{
-  float: left;
-  width: 16%;
-  height: 150px;
-}
-.w-main1 span{
-  display: inline-block;
-  margin-left: 40px;
-  margin-top: 20px;
-  font-size: 18px;
-}
-.w-main2{
-  box-sizing: border-box;
-  border-left: 2px solid #8B0000;
-  text-align: center;
-  float: left;
-  width: 28%;
-  height: 150px;
-}
-.warning-content{
-  padding-top: 25px;
-  width: 40%;
-  margin: auto;
-  text-align: left;
-}
-.change{
-  display: inline-block;
+.wc-title{
   margin-top: 10px;
+  font-size: 16px;
+}
+.show-all{
+  display: inline-block;
+  margin-top: 25px;
+  color: royalblue;
+  font-size: 14px;
+}
+.warning-container{
+  margin-top: 1.25rem;
+}
+.warning-header{
+  margin-left: 1.875rem;
+  width: 10%;
+  float: left;
+}
+.warning-slider{
+  margin-left: 30px;
+  float: left;
+  width: 85%;
+}
+.ws-content{
+  margin-left: 5px;
+  padding-left: 30px;
+  border-left: 1px solid #8B0000;
+  float: left;
+  width: 30%;
+}
+.ws-link{
+  padding-top: 7px;
+  width: 100%;
+  display: inline-block;
+}
+.movement{
+  float: left;
+  width: 10%;
+}
+.details{
+  float: left;
+  width: 85%;
+}
+.product-name{
+  font-weight: bold;
+}
+.product-text{
+  margin-top: 10px;
+}
+.last-update{
+  margin-top:40px;
+  font-size: 10px;
+}
+.warning h3{
+  color: #112b4a;
+}
+a{
+  color: #5a6874;
+  text-decoration: none;
 }
 </style>

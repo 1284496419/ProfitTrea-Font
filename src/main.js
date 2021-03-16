3ca240f0a34364fab107882ad18b892a9cdf2d30
annoming <annoming_2899@163.com>
2021-03-16 10:12:39 +0800

    修改请求地址

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import qs from 'qs'
-axios.defaults.baseURL = "http://localhost:8081/profittrea"
+axios.defaults.baseURL = "http://localhost:8083/profittrea"
 //设置每次请求可以挂在cookies
 axios.default.withCredentials = true
 Vue.config.productionTip = false

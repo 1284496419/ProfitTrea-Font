77d7c3ca8e6656c9c7083d683c782abd19af57af
annoming <annoming_2899@163.com>
2021-05-12 09:46:40 +0800

    修改交易量统计查询逻辑

--- src/components/administrator/Statistics.vue
+++ src/components/administrator/Statistics.vue
@@ -6,18 +6,20 @@
       <!-- <el-breadcrumb-item>交易量列表</el-breadcrumb-item> -->
     </el-breadcrumb>
     <div id="transearch">
-      <span class="search_span">合同编号:</span>
+      <!-- <span class="search_span">合同编号:</span>
       <el-input
         placeholder="根据合同编号查询"
         v-model="tran_id"
         clearable>
-      </el-input>
+      </el-input> -->
       <span class="search_span">股东账号:</span>
       <el-input
         placeholder="根据股东账号查询"
         v-model="stockUser_id"
         clearable>
       </el-input>
+      <el-button type="primary" icon="el-icon-search" @click="selectStatistic">查询</el-button>
+      <el-button icon="el-icon-refresh-left" @click="resetForm">重置</el-button>
     </div>
     <el-table :data="tableData" style="width: 100%" :default-sort="{prop: 'transactionDate', order: 'descending'}">
       <el-table-column prop="transactionDate" label="成交日期" sortable width="180">
@@ -50,13 +52,62 @@
     data() {
       return {
         tableData: [],
-        tran_id:'',
-        stockUser_id:''
+        stockUser_id:'',
+        organizeInfo:''
       }
     },
     methods: {
       formatter(row, column) {
         return row.address;
+      },
+      selectStatistic(){
+        var queryInfo = JSON.stringify({
+          userId:this.stockUser_id,
+          organizationId:this.organizeInfo
+        })
+        this.$axios.post("/transaction/ALL||TRANSACTION.do", queryInfo, {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          }
+        }).then((response) => {
+          if (response.data.code != 100) {
+            this.$message.error(response.data.msg);
+            this.$router.push('/login')
+          } else {
+            this.tableData = []
+            var array = response.data.data
+            array.forEach((item, i) => {
+              this.tableData.push(item)
+            })
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          this.$message.error('查询股东交易信息异常');
+          this.$router.push('/error')
+        })
+
+      },
+      resetForm(){
+        this.stockUser_id = ''
+        var transaction_organize = JSON.stringify({
+          organizationId: this.organizeInfo
+        })
+        this.$axios.post('/transaction/QUERY||ORGANIZETRANSACTION.do', transaction_organize, {
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8'
+          }
+        }).then((response)=>{
+          if (response.data.code != 100) {
+            this.$message.error(response.data.msg);
+            this.$router.push('/login')
+          } else {
+            this.tableData = []
+            this.tableData = response.data.data
+          }
+        }).catch((error)=>{
+          this.$message.error(response.data.msg)
+        })
       }
     },
     mounted() {
@@ -69,16 +120,22 @@
           'Content-Type': 'application/json;charset=UTF-8'
         }
       }).then((response) => {
-        var organizeInfo = response.data.data.organization
+        this.organizeInfo = response.data.data.organization
         var transaction_organize = JSON.stringify({
-          organizationId: organizeInfo
+          organizationId: this.organizeInfo
         })
         this.$axios.post('/transaction/QUERY||ORGANIZETRANSACTION.do', transaction_organize, {
           headers: {
             'Content-Type': 'application/json;charset=UTF-8'
           }
         }).then((response)=>{
-          this.tableData = response.data.data
+          if (response.data.code != 100) {
+            this.$message.error(response.data.msg);
+            this.$router.push('/login')
+          } else {
+            this.tableData = []
+            this.tableData = response.data.data
+          }
         }).catch((error)=>{
           this.$message.error(response.data.msg)
         })
